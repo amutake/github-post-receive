6052a5f5b4cdfa1c723ef49cd8fbc843f1d617ec
amkkun <amkkun@gmail.com>
2013-06-24 14:29:10 +0900

    JSON decode

--- src/Receiver.hs
+++ src/Receiver.hs
@@ -3,6 +3,8 @@
 module Receiver where
 
 import Control.Monad.IO.Class (liftIO)
+import qualified Data.ByteString.Lazy as B
+import Data.Aeson
 import Data.String
 import System.Cmd
 import Web.Scotty
@@ -18,6 +20,10 @@ receiver confs = do
     receiver' (Conf name paths) = do
         let route = fromString $ '/' : name
         post route $ do
-            (msg :: Message) <- jsonData
-            liftIO $ print msg
-            liftIO $ mapM_ system paths
+            str <- body
+            let result = decode $ B.drop 8 str
+            case result of
+                Nothing -> liftIO $ print str
+                Just (msg :: Message) -> do
+                    liftIO $ print msg
+                    liftIO $ mapM_ system paths
