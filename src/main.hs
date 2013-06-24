0daebcf3f13845dfb0536b7721fe7286c48d82c6
amkkun <amkkun@gmail.com>
2013-06-24 12:35:27 +0900

    port

--- src/main.hs
+++ src/main.hs
@@ -1,17 +1,21 @@
 module Main where
 
-import Safe
 import System.Environment
+import System.Exit
 import Web.Scotty
 
 import Conf
 import Receiver
 
+type Port = Int
+
 main :: IO ()
 main = do
     args <- getArgs
-    case headMay args of
-        Nothing -> putStrLn "post-receive conf-file"
-        Just path -> do
-            conf <- readFile path
-            scotty 7777 $ receiver $ parse conf
+    (port, path) <- checkArgs args
+    conf <- readFile path
+    scotty port $ receiver $ parse conf
+
+checkArgs :: [String] -> IO (Port, FilePath)
+checkArgs [port, path] = return (read port, path)
+checkArgs _ = putStrLn "post-receive port conf" >> exitFailure
