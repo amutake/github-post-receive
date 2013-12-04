90335f4e81c32fd914c439bd109392b2954a159a
amutake <amutake.s@gmail.com>
2013-12-05 04:51:30 +0900

    Fix bug of decode url encoding

--- src/Github/PostReceive/Server.hs
+++ src/Github/PostReceive/Server.hs
@@ -2,12 +2,13 @@ module Github.PostReceive.Server
     ( start
     ) where
 
-import Data.Aeson (fromJSON, Result (..), json)
+import Control.Applicative ((<$>))
+import Data.Aeson (decode)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
-import Data.Conduit (($$), ($=), await, yield)
-import Data.Conduit.Attoparsec (sinkParser)
-import Network.HTTP.Types (ok200, badRequest400)
+import Data.Conduit (($$))
+import qualified Data.Conduit.List as CL
+import Network.HTTP.Types (ok200, badRequest400, urlDecode)
 import Network.Wai (requestBody, responseLBS)
 import Network.Wai.Handler.Warp (run, Port)
 
@@ -15,15 +16,7 @@ import Github.PostReceive.Types (Payload)
 
 start :: Port -> (Payload -> IO ()) -> IO ()
 start port cont = run port $ \req -> do
-    value <- requestBody req $= dropC (length "payload=") $$ sinkParser json
-    print value
-    case fromJSON value of
-        Error err -> putStrLn err >> return (responseLBS badRequest400 [] BL.empty)
-        Success payload -> cont payload >> return (responseLBS ok200 [] BL.empty)
-  where
-    dropC count = do
-        mbs <- await
-        case mbs of
-            Just bs | B.length bs > count -> yield $ B.drop count bs
-            Just bs -> dropC $ count - B.length bs
-            Nothing -> return ()
+    bs <- B.drop (length "payload=") . B.concat <$> (requestBody req $$ CL.consume)
+    case decode $ BL.fromStrict $ urlDecode True bs of
+        Nothing -> return (responseLBS badRequest400 [] BL.empty)
+        Just payload -> cont payload >> return (responseLBS ok200 [] BL.empty)
