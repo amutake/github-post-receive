e6e5884e1e6629524003a58d2aa5e715aff0be9a
amutake <amutake.s@gmail.com>
2014-11-06 15:25:15 +0900

    wai==3.*, warp==3.* and remove conduit

--- src/Github/PostReceive/Server.hs
+++ src/Github/PostReceive/Server.hs
@@ -6,8 +6,6 @@ import Control.Applicative ((<$>))
 import Data.Aeson (decode)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
-import Data.Conduit (($$))
-import qualified Data.Conduit.List as CL
 import Network.HTTP.Types (ok200, badRequest400, urlDecode)
 import Network.Wai (requestBody, responseLBS)
 import Network.Wai.Handler.Warp (run, Port)
@@ -15,8 +13,8 @@ import Network.Wai.Handler.Warp (run, Port)
 import Github.PostReceive.Types (Payload)
 
 start :: Port -> (Payload -> IO ()) -> IO ()
-start port cont = run port $ \req -> do
-    bs <- B.drop (length "payload=") . B.concat <$> (requestBody req $$ CL.consume)
+start port cont = run port $ \req respond -> do
+    bs <- B.drop (length "payload=") <$> requestBody req
     case decode $ BL.fromStrict $ urlDecode True bs of
-        Nothing -> return (responseLBS badRequest400 [] BL.empty)
-        Just payload -> cont payload >> return (responseLBS ok200 [] BL.empty)
+        Nothing -> respond (responseLBS badRequest400 [] BL.empty)
+        Just payload -> cont payload >> respond (responseLBS ok200 [] BL.empty)
