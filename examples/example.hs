db60a0b598ada376f4881dfbd81915e9dc504dde
amutake <amutake.s@gmail.com>
2014-11-06 16:27:38 +0900

    routing

--- examples/example.hs
+++ examples/example.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import qualified Data.Map as M
 import Github.PostReceive
 
 main :: IO ()
-main = start 5678 print
+main = start 5678 $ M.fromList [("/", print)]
--- src/Github/PostReceive/Server.hs
+++ src/Github/PostReceive/Server.hs
@@ -1,20 +1,33 @@
 module Github.PostReceive.Server
     ( start
+    , app
     ) where
 
 import Control.Applicative ((<$>))
 import Data.Aeson (decode)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
-import Network.HTTP.Types (ok200, badRequest400, urlDecode)
-import Network.Wai (requestBody, responseLBS)
+import qualified Data.Map as M
+import Network.HTTP.Types (ok200, badRequest400, notFound404, urlDecode, methodPost)
+import Network.Wai (requestBody, responseLBS, Application, rawPathInfo, requestMethod)
 import Network.Wai.Handler.Warp (run, Port)
 
 import Github.PostReceive.Types (Payload)
 
-start :: Port -> (Payload -> IO ()) -> IO ()
-start port cont = run port $ \req respond -> do
-    bs <- B.drop (length "payload=") <$> requestBody req
-    case decode $ BL.fromStrict $ urlDecode True bs of
-        Nothing -> respond (responseLBS badRequest400 [] BL.empty)
-        Just payload -> cont payload >> respond (responseLBS ok200 [] BL.empty)
+start :: Port -> M.Map B.ByteString (Payload -> IO ()) -> IO ()
+start port = run port . app
+
+app :: M.Map B.ByteString (Payload -> IO ()) -> Application
+app routes req respond
+    | method == methodPost = do
+        bs <- B.drop (length "payload=") <$> requestBody req
+        flip (maybe notFound) (M.lookup path routes) $ \cont ->
+            flip (maybe badRequest) (decode $ BL.fromStrict $ urlDecode True bs) $ \payload ->
+                cont payload >> ok
+    | otherwise = notFound
+  where
+    path = rawPathInfo req
+    method = requestMethod req
+    notFound = respond (responseLBS notFound404 [] BL.empty)
+    badRequest = respond (responseLBS badRequest400 [] BL.empty)
+    ok = respond (responseLBS ok200 [] BL.empty)
